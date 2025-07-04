b16c81d05984a5193126d66a1daf01af24c704fb
Volodimyr Bobrovnyk <vladimirbobrovnyk@gmail.com>
2025-07-04 15:17:35 +0300

    Structure for a writing in catalog

--- fat32_emulator.c
+++ fat32_emulator.c
@@ -46,3 +46,20 @@ typedef struct
     uint8_t boot_code[420];
     uint16_t signature;
 } FAT32BootSector;
+
+typedef struct 
+{
+    uint8_t DIR_Name[11];
+    uint8_t DIR_Attr;
+    uint8_t DIR_NTRes;
+    uint8_t DIR_CrtTimeTenth;
+    uint16_t DIR_CrtTime;
+    uint16_t DIR_CrtDate;
+    uint16_t DIR_LstAccDate;
+    uint16_t DIR_FstClusHI;
+    uint16_t DIR_WrtTime;
+    uint16_t DIR_WrtDate;
+    uint16_t DIR_FstClusLO;
+    uint32_t DIR_FileSize;
+} FAT32Directory;
+
